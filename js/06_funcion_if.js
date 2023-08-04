cb3327b6a7d27e7d34fa02495091ffe6cdbf5596
Romulo Felizola <felizolaromulo23@gmail.com>
2023-08-03 22:12:41 -0400

    agrego otro ejercicio del if usando las variables desde el html en inputs

---
+++ js/06_funcion_if.js
@@ -0,0 +1,21 @@
+function CalcularPrecio(NumeroDeEntradas,PrecioDeEntrada) {
+    //creo una variable donde el input del html me proporciuona en valor de la variable usando .value al final de getElementById
+    var NumeroDeEntradas = document.getElementById("NumeroDeEntradas").value
+    var PrecioDeEntrada = document.getElementById("PrecioDeEntrada").value
+    //creo las variables con los distintos descuentos
+    var descuento_2_entradas = 0.10
+    var descuento_3_entradas = 0.30
+    var descuento_5_entradas = 0.50
+     // condiciono la variable de numero de entradas para que se aplique un descuente dependiente del numero de entradas 
+    if (NumeroDeEntradas >= 5){
+        PrecioDeEntrada = PrecioDeEntrada - (PrecioDeEntrada * descuento_5_entradas)
+    }else if (NumeroDeEntradas >= 3){
+        PrecioDeEntrada = PrecioDeEntrada - (PrecioDeEntrada * descuento_3_entradas)
+    }else if (NumeroDeEntradas >= 2){
+        PrecioDeEntrada = PrecioDeEntrada - (PrecioDeEntrada * descuento_2_entradas)
+    } else if(NumeroDeEntradas=1){
+        PrecioDeEntrada = PrecioDeEntrada
+    }
+
+    alert("El precio de tus entradas es = " + PrecioDeEntrada)
+}   
