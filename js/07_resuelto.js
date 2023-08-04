4aebcf601a2395dd4c41d89c62702a2474d0764a
Romulo Felizola <felizolaromulo23@gmail.com>
2023-08-04 04:29:50 -0400

    agrego el ejercicio 07 referente al la validacion del DNI y numeros y letras

---
+++ js/07_resuelto.js
@@ -0,0 +1,23 @@
+function ValidarDni(NumeroDni,LetraDni) {
+
+    var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H',
+'L', 'C', 'K', 'E', 'T']
+    var NumeroDni = document.getElementById("NumeroDni").value
+    var LetraDni = document.getElementById("LetraDni").value
+    
+    if (NumeroDni < 0 || NumeroDni > 9999999 ) {
+        alert("dni invalido")
+        
+    }else if (true){
+      var NumeroLetra = NumeroDni % 23
+    }
+
+    if(LetraDni === letras[NumeroLetra]){      
+                alert("dni valido")
+                
+            }else{
+                alert("letra del dni invalido")
+            }
+           
+}
+        
